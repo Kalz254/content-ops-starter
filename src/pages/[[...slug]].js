ab62183eeeafbc0ea282b9e72b56e679fced2ed2
Kalz254 <kalzchalz254@gmail.com>
2025-12-04 14:04:52 +0300

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -1,11 +1,3 @@
-import React from 'react';
-import Head from 'next/head';
-import { allContent } from '../utils/local-content';
-import { getComponent } from '../components/components-registry';
-import { resolveStaticProps } from '../utils/static-props-resolvers';
-import { resolveStaticPaths } from '../utils/static-paths-resolvers';
-import { seoGenerateTitle, seoGenerateMetaTags, seoGenerateMetaDescription } from '../utils/seo-utils';
-
 function Page(props) {
     const { page, site } = props;
     const { modelName } = page.__metadata;
@@ -16,9 +8,11 @@ function Page(props) {
     if (!PageLayout) {
         throw new Error(`no page layout matching the page model: ${modelName}`);
     }
+
     const title = seoGenerateTitle(page, site);
     const metaTags = seoGenerateMetaTags(page, site);
     const metaDescription = seoGenerateMetaDescription(page, site);
+
     return (
         <>
             <Head>
@@ -26,7 +20,6 @@ function Page(props) {
                 {metaDescription && <meta name="description" content={metaDescription} />}
                 {metaTags.map((metaTag) => {
                     if (metaTag.format === 'property') {
-                        // OpenGraph meta tags (og:*) should be have the format <meta property="og:…" content="…">
                         return <meta key={metaTag.property} property={metaTag.property} content={metaTag.content} />;
                     }
                     return <meta key={metaTag.property} name={metaTag.property} content={metaTag.content} />;
@@ -34,22 +27,14 @@ function Page(props) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 {site.favicon && <link rel="icon" href={site.favicon} />}
             </Head>
-            <PageLayout page={page} site={site} />
+
+            {/* ✅ Wrap your editable content here */}
+            <div data-sb-object-id="page-1">
+                <h1 data-sb-field-path="title">{page.title}</h1>
+                <div data-sb-field-path="body">
+                    <PageLayout page={page} site={site} />
+                </div>
+            </div>
         </>
     );
 }
-
-export function getStaticPaths() {
-    const data = allContent();
-    const paths = resolveStaticPaths(data);
-    return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }) {
-    const data = allContent();
-    const urlPath = '/' + (params.slug || []).join('/');
-    const props = await resolveStaticProps(urlPath, data);
-    return { props };
-}
-
-export default Page;
