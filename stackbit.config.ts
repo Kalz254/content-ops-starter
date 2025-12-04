2cfbc43347d884ddce481051f44d65e522bc1b15
Kalz254 <kalzchalz254@gmail.com>
2025-12-04 12:32:43 +0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,58 +1,24 @@
-import { defineStackbitConfig, DocumentStringLikeFieldNonLocalized, SiteMapEntry } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
-import { allModels } from 'sources/local/models';
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
-const gitContentSource = new GitContentSource({
-    rootPath: __dirname,
-    contentDirs: ['content'],
-    models: Object.values(allModels),
-    assetsConfig: {
-        referenceType: 'static',
-        staticDir: 'public',
-        uploadDir: 'images',
-        publicPath: '/'
-    }
+export default defineStackbitConfig({
+  stackbitVersion: "0.1.0", // required
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Post",
+          type: "page",          // mark as page
+          urlPath: "/{slug}",    // connect model to URL
+          filePath: "content/pages/{slug}.md",
+          fields: [
+            { name: "title", type: "string", required: true },
+            { name: "markdown_content", type: "markdown_content" }
+          ]
+        }
+      ]
+    })
+  ]
 });
-
-export const config = defineStackbitConfig({
-    stackbitVersion: '~0.7.0',
-    ssgName: 'nextjs',
-    nodeVersion: '18',
-    styleObjectModelName: 'ThemeStyle',
-    contentSources: [gitContentSource],
-    presetSource: {
-        type: 'files',
-        presetDirs: ['sources/local/presets']
-    },
-    siteMap: ({ documents, models }): SiteMapEntry[] => {
-        const pageModels = models.filter((model) => model.type === 'page').map((model) => model.name);
-        return documents
-            .filter((document) => pageModels.includes(document.modelName))
-            .map((document) => {
-                let slug = (document.fields.slug as DocumentStringLikeFieldNonLocalized)?.value;
-                if (!slug) return null;
-                /* Remove the leading slash in order to generate correct urlPath
-                regardless of whether the slug is '/', 'slug' or '/slug' */
-                slug = slug.replace(/^\/+/, '');
-                switch (document.modelName) {
-                    case 'PostFeedLayout':
-                        return {
-                            urlPath: '/blog',
-                            document: document
-                        };
-                    case 'PostLayout':
-                        return {
-                            urlPath: `/blog/${slug}`,
-                            document: document
-                        };
-                    default:
-                        return {
-                            urlPath: `/${slug}`,
-                            document: document
-                        };
-                }
-            });
-    }
-});
-
-export default config;
